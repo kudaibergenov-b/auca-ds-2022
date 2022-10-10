ae6b9301e94add720608650bab918693a22150b8
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2022-10-10 15:11:06 +0200

    lab02 p13 wrong answer

---
+++ lab02/p13/main.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+template <typename C>
+int sz(const C &c) { return static_cast<int>(c.size()); }
+
+using namespace std;
+
+int main()
+{
+    iostream::sync_with_stdio(false);
+
+    for (int b, n; cin >> b >> n && b != 0 && n != 0;)
+    {
+        vector<int> banks;
+        bool canPay = true;
+        for (int i = 0; i < b; i++)
+        {
+            int money;
+            cin >> money;
+            banks.push_back(money);
+        }
+        for (int k = 0; k < n; k++)
+        {
+            
+            int d, c, v;
+            cin >> d >> c >> v;
+            if (b < v)
+            {
+                canPay = false;
+            }
+        }
+
+        cout << (canPay ? "S" : "N") << endl;
+    } 
+}

c0f410b763b761eae7d41e5281b8611f4bd9fd79
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2022-10-17 11:33:13 +0200

    lab01 p06 some tests

--- lab01/p06/main.cpp
+++ lab01/p06/main.cpp
@@ -5,6 +5,26 @@
 
 using namespace std;
 
+template <typename C>
+string containerToStr(const C &c)
+{
+    ostringstream sout;
+    bool isFirst = true;
+    sout << "{";
+    for (const auto &e : c)
+    {
+        if (!isFirst)
+        {
+            sout << ", ";
+        }
+        sout << e;
+        isFirst = false;
+    }
+    sout << "}";
+
+    return sout.str();
+}
+
 TEST_CASE("Default constructor")
 {
     vector<int> v;
@@ -13,18 +33,59 @@ TEST_CASE("Default constructor")
     REQUIRE(v.size() == 0);
 }
 
+TEST_CASE("Constructor with size")
+{
+    vector<int> v(10);
+
+    REQUIRE(v.size() == 10);
+
+    for (auto i : v)
+    {
+        REQUIRE(v[i] == 0);
+    }
+}
+
+TEST_CASE("Constructor with size and initial value")
+{
+    vector<int> v(10, 1);
+
+    REQUIRE(v.size() == 10);
+
+    for (auto i : v)
+    {
+        REQUIRE(v[i] == 1);
+    }
+}
+
 TEST_CASE("Copy Constructor")
 {
     vector<int> v = {1, 2, 3, 4, 5};
 
-    //TODO
+    vector<int> v2 = v;
+
+    REQUIRE(v2.size() == 5);
+    REQUIRE(containerToStr(v2) == "{1, 2, 3, 4, 5}");
+
+    v[0] = 1000;
+    v2[4] = 42;
+
+    REQUIRE(containerToStr(v) == "{1000, 2, 3, 4, 5}");
+    REQUIRE(containerToStr(v2) == "{1, 2, 3, 4, 42}");
 }
 
 TEST_CASE("push back")
 {
     vector<int> v;
 
-    //TODO
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+
+    REQUIRE(v.size() == 5);
+    REQUIRE(containerToStr(v) == "{1, 2, 3, 4, 5}");
+    REQUIRE(v.capacity() == 8);
 }
 
 TEST_CASE("back()")
@@ -60,7 +121,7 @@ TEST_CASE("[]")
     REQUIRE(v[0] == 100);
 }
 
-TEST_CASE("assignment()")
+TEST_CASE("Assignment operator")
 {
     vector<int> v = {1, 2, 3};
     vector<int> c = {3, 4, 5};
@@ -69,7 +130,11 @@ TEST_CASE("assignment()")
 
     REQUIRE(c[0] == 1);
 
-    //TODO
+    v[2] = 34;
+    c[0] = 100;
+
+    REQUIRE(containerToStr(v) == "{1, 2, 34}");
+    REQUIRE(containerToStr(c) == "{100, 2, 3}");
 
     v = vector<int>();
 

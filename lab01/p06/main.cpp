18056301c06a201b6370fdc4750ac478b98e1210
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2022-10-18 20:29:50 +0200

    lab01 p06 almost completed

--- lab01/p06/main.cpp
+++ lab01/p06/main.cpp
@@ -53,7 +53,7 @@ TEST_CASE("constructor with size and initial value")
 
     for (auto i : v)
     {
-        REQUIRE(v[i] == 1);
+        REQUIRE(i == 1);
     }
 }
 
@@ -161,6 +161,19 @@ TEST_CASE("pop_back")
     REQUIRE(v.size() == 3);
 }
 
+TEST_CASE("auto element : container")
+{
+    vector<int> v = {1, 2, 3};
+    int t = 0;
+
+    for (auto i : v)
+    {
+        t += i;
+    }
+
+    REQUIRE(t == 6);
+}
+
 TEST_CASE("back()")
 {
     vector<int> v = {1, 2, 3};
@@ -183,3 +196,61 @@ TEST_CASE("front()")
     REQUIRE(v.front() == 100);
 }
 
+TEST_CASE("type iterator, operators")
+{
+    // TODO;
+}
+
+TEST_CASE("insert(it, value), insert(it, beg, end")
+{
+    // TODO;
+}
+
+TEST_CASE("erase(it), erase(beg, end)")
+{
+    // TODO;
+}
+
+TEST_CASE("constructor vector(beg, end)")
+{
+    // TODO;
+}
+
+TEST_CASE("reverse")
+{
+    vector<int> v = {1, 2, 3, 4, 5};
+
+    reverse(v.begin(), v.end());
+
+    REQUIRE(containerToStr(v) == "{5, 4, 3, 2, 1}");
+}
+
+TEST_CASE("sort")
+{
+    vector<int> v = {3, 2, 4, 1, 5};
+
+    sort(v.begin(), v.end());
+
+    REQUIRE(containerToStr(v) == "{1, 2, 3, 4, 5}");
+}
+
+TEST_CASE("binary_search")
+{
+    vector<int> v = {10, 10, 10, 10, 10};
+
+    REQUIRE(binary_search(v.begin(), v.end(), 10) == true);
+}
+
+TEST_CASE("min_element")
+{
+    vector<int> v = {2, 4, 5, 7, 3};
+
+    REQUIRE(*min_element(v.begin(), v.end()) == 2);
+}
+
+TEST_CASE("max_element")
+{
+    vector<int> v = {2, 4, 5, 7, 3};
+
+    REQUIRE(*max_element(v.begin(), v.end()) == 7);
+}

8f98e94a6e72711c8fc112c114d44ed7da6213b6
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2022-11-18 20:43:22 +0100

    project01 some work

--- project01/BigInt/BigInt.hpp
+++ project01/BigInt/BigInt.hpp
@@ -9,8 +9,49 @@
 class BigInt
 {
     friend std::ostream &operator<<(std::ostream &out, const BigInt &x);
+    friend BigInt operator+(const BigInt &x, const BigInt &y);
     std::vector<int> mDigits;
     bool mIsNegative;
+
+    static BigInt addAbsValues(const BigInt &x, const BigInt &y)
+    {
+        auto itX = x.mDigits.rbegin();
+        auto itY = y.mDigits.rbegin();
+
+        BigInt z;
+        z.mDigits.resize(std::max(x.mDigits.size(), y.mDigits.size()) + 1);
+        auto itZ = z.mDigits.rbegin();
+
+        int carry = 0;
+
+        while (itX != x.mDigits.rend() || itY != y.mDigits.rend())
+        {
+            int s = carry;
+            if (itX != x.mDigits.rend())
+            {
+                s += *itX;
+                ++itX;
+            }
+            if (itY != y.mDigits.rend())
+            {
+                s += *itY;
+                ++itY;
+            }
+            *itZ = s % 10;
+            carry = (s > 9) ? 1 : 0;
+            ++itZ;
+        }
+        if (carry != 0)
+        {
+            *itZ = carry;
+        }
+        if (z.mDigits.size() > 1 && z.mDigits.front() == 0)
+        {
+            z.mDigits.erase(z.mDigits.begin());
+        }
+
+        return z;
+    }
 
 public:
     BigInt()
@@ -37,11 +78,12 @@ public:
         {
             mIsNegative = true;
             index++;
-        } else if (isdigit(s.at(index))) 
+        } 
+        else if (isdigit(s.at(index))) 
         {
             mDigits.push_back(s.at(index) - '0');
             index++;
-        }
+        } 
 
         for (int i = index; i < (int)s.length(); i++)
         {
@@ -55,7 +97,6 @@ public:
             }
         }
     }
-    
 };
 
 inline std::ostream &operator<<(std::ostream &out, const BigInt &x)
@@ -71,4 +112,14 @@ inline std::ostream &operator<<(std::ostream &out, const BigInt &x)
     }
 
     return out;
+}
+
+inline BigInt operator+(const BigInt &x, const BigInt &y)
+{
+    if (!x.mIsNegative && !y.mIsNegative)
+    {
+        return BigInt::addAbsValues(x, y);
+    }
+    
+    throw std::runtime_error("not implemented yet");
 }
--- project01/BigInt/main.cpp
+++ project01/BigInt/main.cpp
@@ -28,14 +28,14 @@ TEST_CASE("Default constructor")
 TEST_CASE("Constructor with a string")
 {
     ostringstream sout;
-    SUBCASE("positive number")
+    SUBCASE("positive number, test #1")
     {
         BigInt x("123456789123456789");
         sout << x;
         REQUIRE(sout.str() == "123456789123456789");
     }
 
-    SUBCASE("positive number")
+    SUBCASE("positive number, test #2")
     {
         BigInt x("+123456789123456789");
         sout << x;
@@ -54,5 +54,65 @@ TEST_CASE("Constructor with a string")
         REQUIRE_THROWS_AS(BigInt(""), runtime_error);
     }
 
+    SUBCASE(" 123")
+    {
+        REQUIRE_THROWS_AS(BigInt(" 123"), runtime_error);
+        REQUIRE_THROWS_WITH(BigInt(" 123"), "Incorrect format of BigInteger");
+    }
 
+    SUBCASE("123-123")
+    {
+        REQUIRE_THROWS_AS(BigInt("123-123"), runtime_error);
+        REQUIRE_THROWS_WITH(BigInt("123-123"), "Incorrect format of BigInteger");
+    }
+
+    SUBCASE("hello")
+    {
+        REQUIRE_THROWS_AS(BigInt("hello"), runtime_error);
+        REQUIRE_THROWS_WITH(BigInt("hello"), "Incorrect format of BigInteger");
+    }
+
+    // SUBCASE("0000213")
+    // {
+    //     BigInt x("0000213");
+    //     sout << x;
+    //     REQUIRE(sout.str() == "213");
+    // }
 }
+
+TEST_CASE("Addition")
+{
+    ostringstream sout;
+
+    SUBCASE("positive + positive, test #1")
+    {
+        BigInt x("193");
+        BigInt y("59");
+        sout << x + y;
+        REQUIRE(sout.str() == "252");
+    }
+
+    SUBCASE("positive + positive, test #2")
+    {
+        BigInt x("999");
+        BigInt y("1");
+        sout << x + y;
+        REQUIRE(sout.str() == "1000");
+    }
+
+    SUBCASE("positive + positive, test #3")
+    {
+        for (int x = 0; x <= 1000; ++x)
+        {
+            for (int y = 0; y <= 1000; ++y)
+            {
+                BigInt a(to_string(x));
+                BigInt b(to_string(y));
+                sout << a + b;
+                REQUIRE(sout.str() == to_string(x + y));
+                sout.str("");
+            }
+        }
+    }
+}
+

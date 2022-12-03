5f15879040e91721b349eab581c39f0aa911eaf6
Bakyt Kudaibergenov <kudaibergenov_b@auca.kg>
2022-12-03 15:53:43 +0100

    lab06 p01, p02, p03, p06 completed

---
+++ au/algol.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <utility>
+
+template <typename T>
+void auSwap(T &x, T &y)
+{
+    T t = std::move(x);
+    x = std::move(y);
+    y = std::move(t);
+}
+
+template <typename BidirectionalIter>
+void auReverse(BidirectionalIter beg, BidirectionalIter end)
+{
+    for (;;)
+    {
+        if (beg == end)
+        {
+            break;
+        }
+
+        --end;
+
+        if (beg == end)
+        {
+            break;
+        }
+
+        auSwap(*beg, *end);
+
+        ++beg;
+    }
+}
+
+template <typename ForwardIter, typename Key>
+ForwardIter auFind(ForwardIter beg, ForwardIter end, const Key &key)
+{
+    for (; beg != end; ++beg)
+    {
+        if (*beg == key)
+        {
+            return beg;
+        }
+    }
+
+    return beg;
+}
+
+template <typename ForwardIter, typename UnaryPredicate>
+ForwardIter auFindIf(ForwardIter beg, ForwardIter end, UnaryPredicate pred)
+{
+    for (; beg != end; ++beg)
+    {
+        if (pred(*beg))
+        {
+            return beg;
+        }
+    }
+
+    return beg;
+}
